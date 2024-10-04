f9f89e0366d64485e2456b9dcfd55aec778d56dd
Alexandr_Tikhonov <tihas75@mail.ru>
2024-10-04 22:33:50 +0300

    add module_2_1

--- module_2/module_2_1_"Поток вывода команд".py
+++ module_2/module_2_1_"Поток вывода команд".py
@@ -1,4 +1,4 @@
-number = int(input('Введите число: ')) # Fizz, Buzz, FizzBuzz
+number = int(input('Введите число: ')) # Fizz, Buzz, FizzBuzz,
 if number % 3 == 0 and number % 5 == 0:
     print('FizzBuzz')
 elif number % 3 == 0:
