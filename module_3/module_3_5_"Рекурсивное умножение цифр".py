fbf2cd53449b864114e7652b743351a82bfe316e
Alexandr_Tikhonov <tihas75@mail.ru>
2024-10-14 22:27:29 +0300

    add module_3_5

---
+++ module_3/module_3_5_"Рекурсивное умножение цифр".py
@@ -0,0 +1,13 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    # print(first)
+    if len(str_number) > 1:
+        # print(str_number)
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+
+result = get_multiplied_digits(40203)
+print(result)
