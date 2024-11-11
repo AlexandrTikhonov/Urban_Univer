606f29baba20a270037308e313ba1734c7862f4d
Alexandr_Tikhonov <tihas75@mail.ru>
2024-11-11 20:30:47 +0300

    fix ales

--- module_3/module_3_3_"Распаковка".py
+++ module_3/module_3_3_"Распаковка".py
@@ -1,20 +1,20 @@
-# 1. Функция с параметрами по умолчанию:
+### 1. Функция с параметрами по умолчанию:
 def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
-# 2. Распаковка параметров:
+### 2. Распаковка параметров:
 values_list = [1, 'item', True]
 values_dict = {'a': 1, 'b': 'буква', 'c': True}
 
-# 3. Распаковка + отдельные параметры:
+## 3. Распаковка + отдельные параметры:
 values_list_2 = [3, '5']
 
-print_params()
-print_params(9)
-print_params(1, 2)
-print_params(b=25)
-print_params(c=[1, 2, 3])
+# print_params()
+# print_params(9)
+# print_params(1, 2)
+# print_params(b=25)
+# print_params(c=[1, 2, 3])
 print_params(*values_list)
 print_params(**values_dict)
 print_params(*values_list_2, 42)
