d9c9692eb25b10a71ec7b60ff0da56d510cc4aa2
Alexandr_Tikhonov <tihas75@mail.ru>
2024-11-21 20:18:24 +0300

    add module_7_1 and 7_2

---
+++ module_7/module_7_1_"Строки байты и кодировки".py
@@ -0,0 +1,27 @@
+# print('hello')  # ASCII
+# print(ord('a'))
+#
+# ### ----------------------
+# a = 'hello'
+# chars = []
+# for i in a:
+#     chars.append(ord(i))
+#
+# print(chars)
+#
+# ### ---------------------
+# s = ''
+# for i in chars:
+#     s += chr(i)
+#
+# print(s)
+#
+# ### ------------
+# for i in range(1000, 1200):
+#     print(chr(i))
+
+### ---------------
+print(hex(ord('h')))
+bb = b'\x68'
+print(type(bb))
+print(bb.decode())
